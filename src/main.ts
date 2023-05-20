1e64e0ef17ec4331000df7991ca1c3bdef613d28
Tom Fowler <tcd.fowler@gmail.com>
2023-05-21 00:36:47 +0100

    Lesson 6 complete

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,115 @@
-type One = string;
-type Two = string | number;
-type Three = 'hello';
-
-// convert to more or less specific
-let a: One = 'hello';
-let b: Two = a as Two ; // less specific type
-let c = a as Three; // more specific
-
-// angle brackets cannot be used in tsx files (i.e. with react)
-let d = <One>'world'
-let e = <string | number>'world'
-
-const addOrConcat = (a: number, b: number, c: 'add' | 'concat'): number | string => {
-    if( c === 'add') return a + b;
-    return '' + a + b;
+class Coder {
+    constructor(
+        public readonly name:string,
+        public music: string,
+        private age: number ,
+        protected lang: string = 'TypeScript'
+        ) {
+        this.name = name
+        this.music = music
+        this.age = age
+        this.lang = lang
+    }
+    
+    public getAge() {
+        return `Hello, I'm ${this.age}`;
+    }
 }
 
-let myVal: string = addOrConcat(2,2,'concat') as string;
+const Tom = new Coder('Tom', 'R&B', 28)
+console.log(Tom.getAge());
+// console.log(Tom.age);
+// console.log(Tom.lang);
 
-// Be careful! TS sees no prob - but a string is returned
-let nextVal: number = addOrConcat(2,2,'concat') as number;
+class WebDev extends Coder {
+    constructor(public computer: string,
+        name:string,
+        music: string,
+        age: number
+        ) {
+            super(name, music, age)
+            this.computer = computer
+    }
+    public getLang() {
+        return `I write ${this.lang}`
+    }
+}
 
-// 10 as string
-(10 as unknown) as string
+const Sara = new WebDev('Mac', 'Sara', 'Lofi', 25)
+console.log(Sara.getLang());
+// console.log(Sara.age);
+// console.log(Sara.lang);
+//////////////////////////////////
 
-// The DOM
-const img = document.querySelector('img')!;
-const myImg = document.getElementById('#img') as HTMLImageElement;
-const nextImg = <HTMLImageElement>document.getElementById('#img');
+interface Musician {
+    name: string,
+    instrument: string,
+    play(action: string): string;
+}
 
-img.src
-myImg.src
+class Guitarist implements Musician {
+    name: string;
+    instrument: string;
+
+    constructor(name: string, instrument: string) {
+        this.name = name;
+        this.instrument = instrument
+    }
+
+    play(action: string){
+        return `${this.name} ${action} the ${this.instrument}`
+    }
+}
+
+const Page = new Guitarist('Jimmy', 'guitar');
+
+console.log(Page.play('strums'))
+////////////////////////////////
+
+class Peeps {
+    static count: number = 0;
+    static getCount(): number {
+        return Peeps.count
+    }
+    public id: number
+    constructor(public name: string){
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+
+}
+
+const John = new Peeps('John');
+const Steve = new Peeps('Steve');
+const Amy = new Peeps('Amy');
+
+console.log(Amy.id)
+console.log(Steve.id)
+console.log(John.id)
+console.log(Peeps.count);
+////////////////////////////////////////
+
+class Bands {
+    private dataState: string[]
+
+    constructor(){
+        this.dataState = [];
+    }
+    public get data(): string[] {
+        return this.dataState
+    }
+
+    public set data(value: string[]) {
+        if(Array.isArray(value) && value.every(el => typeof el === 'string')){
+            this.dataState = value
+            return
+        } else throw new Error('Params is not an array of strings')
+    }
+}
+
+const MyBands = new Bands()
+MyBands.data = ['Neil Young', 'Led Zep']
+console.log(MyBands.data)
+MyBands.data = [...MyBands.data, 'ZZ Top']
+console.log(MyBands.data)
+MyBands.data = ['Van Halen', 5150]
